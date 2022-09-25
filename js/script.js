86485f9f7b5eafeef9a2a2d137c0442d0666b2a6
muallimim <muallimim@yahoo.com>
2022-09-25 22:06:39 +0300

    project completed

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 document.getElementById('rock').addEventListener('click', rock);
+document.getElementById('paper').addEventListener('click', paper);
+document.getElementById('scissors').addEventListener('click', scissors);
 
 ////////////////////////////////////////////////////
 function rock(event){
@@ -37,6 +39,86 @@ function rock(event){
     }    
 
 document.getElementById('result').innerHTML = 'Sizin Seçiminiz: Rock'
+                                             +'<br> Bilgisayarın Seçimi: '+ pc_choice_text
+                                             +'<br> Sonuç:'+result_text;
+}
+/////////////////////////////////
+function paper(event){
+    event.preventDefault();
+
+    var pc_choice;
+    var pc_choice_text;
+
+    var rock_image;
+    var paper_image;
+    var scissors_image;
+    var result_text;
+
+    rock_image ='https://upload.wikimedia.org/wikipedia/commons/7/7e/Rock-paper-scissors_%28rock%29.png';
+    paper_image ='https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Rock-paper-scissors_%28paper%29.png/1200px-Rock-paper-scissors_%28paper%29.png';
+    scissors_image= 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Rock-paper-scissors_%28scissors%29.png/1024px-Rock-paper-scissors_%28scissors%29.png';
+
+    pc_choice = Math.floor(Math.random() * 3) + 1;
+
+    if(pc_choice==1){
+        pc_choice_text = 'Rock';
+        document.getElementById('img').src = rock_image;
+        result_text = 'Yendiniz';
+    }
+
+    if(pc_choice==2){
+        pc_choice_text = 'Paper'; 
+        document.getElementById('img').src = paper_image;
+        result_text = 'Berabere';
+    }
+
+    if(pc_choice==3){
+        pc_choice_text = 'Scissors';   
+        document.getElementById('img').src = scissors_image;
+        result_text = 'Yenildiniz';
+    }    
+
+document.getElementById('result').innerHTML = 'Sizin Seçiminiz: Paper'
+                                             +'<br> Bilgisayarın Seçimi: '+ pc_choice_text
+                                             +'<br> Sonuç:'+result_text;
+}
+////////////////////////////////////
+function scissors(event){
+    event.preventDefault();
+
+    var pc_choice;
+    var pc_choice_text;
+
+    var rock_image;
+    var paper_image;
+    var scissors_image;
+    var result_text;
+
+    rock_image ='https://upload.wikimedia.org/wikipedia/commons/7/7e/Rock-paper-scissors_%28rock%29.png';
+    paper_image ='https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Rock-paper-scissors_%28paper%29.png/1200px-Rock-paper-scissors_%28paper%29.png';
+    scissors_image= 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Rock-paper-scissors_%28scissors%29.png/1024px-Rock-paper-scissors_%28scissors%29.png';
+
+    pc_choice = Math.floor(Math.random() * 3) + 1;
+
+    if(pc_choice==1){
+        pc_choice_text = 'Rock';
+        document.getElementById('img').src = rock_image;
+        result_text = 'Yenildiniz';
+    }
+
+    if(pc_choice==2){
+        pc_choice_text = 'Paper'; 
+        document.getElementById('img').src = paper_image;
+        result_text = 'Yendiniz';
+    }
+
+    if(pc_choice==3){
+        pc_choice_text = 'Scissors';   
+        document.getElementById('img').src = scissors_image;
+        result_text = 'Berabere';
+    }    
+
+document.getElementById('result').innerHTML = 'Sizin Seçiminiz: Scissors'
                                              +'<br> Bilgisayarın Seçimi: '+ pc_choice_text
                                              +'<br> Sonuç:'+result_text;
 }
